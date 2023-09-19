60f6173273c51c89538b5d2ca5051f6a8d0a2d5d
Subhendu Dash <71781104+subhendudash02@users.noreply.github.com>
2023-09-19 11:32:16 +0000

    added post route

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -8,10 +9,34 @@ app.use((req, res, next) => {
     next();
 });
 
-// app.post('/bfhl', (req, res) => {
-//     console.log(res.body);
-//     res.send(req.body);
-// });
+app.use(bodyParser.json());
+
+app.post('/bfhl', (req, res) => {
+    let arr = req.body.data;
+    let nums = [];
+    let alpha = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (isNaN(arr[i]) === false) {
+            nums.push(arr[i]);
+        }
+        else {
+            alpha.push(arr[i]);
+        }
+    }
+
+    let highest = alpha.sort().pop();
+
+    res.send({
+        "is_success": true,
+        "user_id": "subhendudash02",
+        "email": "subhendu.dash2020@vitstudent.ac.in",
+        "roll_number": "20BEE1004",
+        "numbers": nums,
+        "alphabets": alpha,
+        "highest_alphabet": [highest]
+    });
+});
 
 app.get('/bfhl', (req, res) => {
     res.send({'operation_code': 1});
