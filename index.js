0c3e4dd79b0a4abd094945aa32cea9913b22c03e
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2023-09-19 18:30:46 +0530

    error handling in api

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyParser = require('body-parser');
-
 const app = express();
 
 app.use((req, res, next) => {
@@ -11,32 +10,42 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post('/bfhl', (req, res) => {
+app.post('/bfhl', (req, res, next) => {
     let arr = req.body.data;
     let nums = [];
     let alpha = [];
 
-    for (let i = 0; i < arr.length; i++) {
-        if (isNaN(arr[i]) === false) {
-            nums.push(arr[i]);
-        }
-        else {
-            alpha.push(arr[i]);
-        }
+    if (arr === undefined) {
+        res.status(500);
+        res.send({"is_success": false, "error": "No data found"});
     }
-
-    alpha.sort();
-    let highest = alpha[alpha.length - 1]; 
-
-    res.send({
-        "is_success": true,
-        "user_id": "subhendu_dash_29102002",
-        "email": "subhendu.dash2020@vitstudent.ac.in",
-        "roll_number": "20BEE1004",
-        "numbers": nums,
-        "alphabets": alpha,
-        "highest_alphabet": [highest]
-    });
+    else if (typeof arr !== "object") {
+        res.status(500);
+        res.send({"is_success": false, "error": "Data is not in array format"});
+    }
+    else {
+        for (let i = 0; i < arr.length; i++) {
+            if (isNaN(arr[i]) === false) {
+                nums.push(arr[i]);
+            }
+            else {
+                alpha.push(arr[i]);
+            }
+        }
+    
+        alpha.sort();
+        let highest = alpha[alpha.length - 1]; 
+    
+        res.send({
+            "is_success": true,
+            "user_id": "subhendu_dash_29102002",
+            "email": "subhendu.dash2020@vitstudent.ac.in",
+            "roll_number": "20BEE1004",
+            "numbers": nums,
+            "alphabets": alpha,
+            "highest_alphabet": [highest]
+        });
+    }
 });
 
 app.get('/bfhl', (req, res) => {
